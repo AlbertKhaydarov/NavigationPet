2dc64a7f726b3351e17255b76975827489426f8b
AlbertKhaydarov <aghaydarov@gmail.com>
2023-10-27 14:29:13 +0300

    Initial Commit

---
+++ NavigationPet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationPet
+//
+//  Created by Альберт Хайдаров on 27.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
